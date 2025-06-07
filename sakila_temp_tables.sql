47ff15a8c25d30979d4adba05f00c5dcedb43847
Juana <juana.gabriela21@gmail.com>
2025-06-07 21:37:07 +0200

    solved labç

---
+++ sakila_temp_tables.sql
@@ -0,0 +1,39 @@
+USE sakila;
+#1
+CREATE OR REPLACE VIEW customer_rental_summary AS
+SELECT 
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    c.email,
+    COUNT(r.rental_id) AS rental_count
+FROM customer c
+JOIN rental r ON c.customer_id = r.customer_id
+GROUP BY c.customer_id;
+SELECT * FROM customer_rental_summary LIMIT 5;
+
+#2
+CREATE TEMPORARY TABLE customer_payment_summary AS
+SELECT 
+    crs.customer_id,
+    SUM(p.amount) AS total_paid
+FROM customer_rental_summary crs
+JOIN payment p ON crs.customer_id = p.customer_id
+GROUP BY crs.customer_id;
+
+#3
+WITH customer_summary_cte AS (
+    SELECT 
+        crs.first_name,
+        crs.last_name,
+        crs.email,
+        crs.rental_count,
+        cps.total_paid,
+        ROUND(cps.total_paid / crs.rental_count, 2) AS average_payment_per_rental
+    FROM customer_rental_summary crs
+    JOIN customer_payment_summary cps ON crs.customer_id = cps.customer_id
+)
+
+SELECT *
+FROM customer_summary_cte
+ORDER BY total_paid DESC;
